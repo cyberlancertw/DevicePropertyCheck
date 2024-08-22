d86af66eba2992f63c6716985550aa2b187a171c
cyberlancer <cyberlancer@gmail.com>
2024-08-23 00:29:20 +0800

    增加棄用的旋轉判斷

--- function.js
+++ function.js
@@ -1,5 +1,12 @@
 var timeoutResize;
-var timeoutOrientationChange;
+var timeoutScreenOrientationChange;
+var timeoutWindowOrientationChange;
+var support = {
+    'windowScreenOrientation': false,
+    'windowDeviceOrientation': false,
+    'windowOrientation': false,
+    'windowMatchMedia': false
+};
 
 window.addEventListener('load', BodyInit);
 
@@ -9,9 +16,41 @@ window.addEventListener('load', BodyInit);
 function BodyInit(){
     if (window.screen.orientation){
         window.screen.orientation.addEventListener('change', ScreenOrientationChange);
+        support.windowScreenOrientation = true;
+    }
+    else{
+        document.getElementById('fireScreenOrientationChange').querySelector('td').textContent = 'Screen API: orientationchange event not supported';
+        document.getElementById('screenOrientationType').textContent = 'not supported'
     }
     window.addEventListener('resize', WindowResize);
-    window.addEventListener('deviceorientation', WindowDeviceOrientation);
+
+    if (window.DeviceOrientationEvent){
+        window.addEventListener('deviceorientation', WindowDeviceOrientation);
+        support.windowDeviceOrientation = true;
+    }
+    else{
+        document.getElementById('trDeviceOrientationEventAlpha').className = 'hidden';
+        document.getElementById('trDeviceOrientationEventBeta').className = 'hidden';
+        document.getElementById('trDeviceOrientationEventGamma').className = 'hidden';
+        document.getElementById('fireWindowDeviceOrientation').querySelector('td').textContent = 'DeviceOrientationEvent API not supported'
+    }
+
+    if (window.orientation !== undefined){
+        window.addEventListener('orientationchange', WindowOrientationChange);
+        support.windowOrientation = true;
+    }
+    else{
+        document.getElementById('trWindowOrientation').className = 'hidden';
+        document.getElementById('fireWindowOrientationChange').className = 'hidden';
+    }
+
+    if (window.matchMedia){
+        support.windowMatchMedia = true;
+    }
+    else{
+        document.getElementById('windowMatchMediaPortrait').textContent = 'not supported';
+        document.getElementById('windowMatchMediaLandscape').textContent = 'not supported';
+    }
     Refresh();
 }
 
@@ -39,13 +78,31 @@ function WindowResize(){
 function ScreenOrientationChange(){
     document.getElementById('fireScreenOrientationChange').className = 'fire';
     // 若有等待中的 setTimeout，清除掉，後面再建立，等於一直延後
-    if (timeoutOrientationChange){
-        window.clearTimeout(timeoutOrientationChange);
-        timeoutOrientationChange = undefined;
+    if (timeoutScreenOrientationChange){
+        window.clearTimeout(timeoutScreenOrientationChange);
+        timeoutScreenOrientationChange = undefined;
     }
 
-    timeoutOrientationChange = window.setTimeout(function(){
+    timeoutScreenOrientationChange = window.setTimeout(function(){
         document.getElementById('fireScreenOrientationChange').className = '';
+    }, 100);
+
+    Refresh();
+}
+
+/**
+ * 棄用的旋轉裝置的事件
+ */
+function WindowOrientationChange(){
+    document.getElementById('fireWindowOrientationChange').className = 'fire';
+    // 若有等待中的 setTimeout，清除掉，後面再建立，等於一直延後
+    if (timeoutWindowOrientationChange){
+        window.clearTimeout(timeoutWindowOrientationChange);
+        timeoutWindowOrientationChange = undefined;
+    }
+
+    timeoutWindowOrientationChange = window.setTimeout(function(){
+        document.getElementById('fireWindowOrientationChange').className = '';
     }, 100);
 
     Refresh();
@@ -56,17 +113,11 @@ function ScreenOrientationChange(){
  * @param {DeviceOrientationEvent} event 事件
  */
 function WindowDeviceOrientation(event){
-    if (!event.alpha && !event.beta && !event.gamma){
-        document.getElementById('trDeviceOrientationEventAlpha').className = 'hidden';
-        document.getElementById('trDeviceOrientationEventBeta').className = 'hidden';
-        document.getElementById('trDeviceOrientationEventGamma').className = 'hidden';
+    if (!support.windowDeviceOrientation){
         return;
     }
-    document.getElementById('trDeviceOrientationEventAlpha').className = '';
     document.getElementById('deviceOrientationEventAlpha').textContent = getValue(event.alpha);
-    document.getElementById('trDeviceOrientationEventBeta').className = '';
     document.getElementById('deviceOrientationEventBeta').textContent = getValue(event.beta);
-    document.getElementById('trDeviceOrientationEventGamma').className = '';
     document.getElementById('deviceOrientationEventGamma').textContent = getValue(event.gamma);
 }
 
@@ -75,21 +126,17 @@ function WindowDeviceOrientation(event){
  */
 function Refresh(){
     document.getElementById('navigatorUserAgent').textContent = window.navigator.userAgent;
-    if (window.orientation !== undefined){
+    if (support.windowOrientation){
         document.getElementById('windowOrientation').textContent = getValue(window.orientation);
-        document.getElementById('trWindowOrientation').className = '';
     }
-    else{
-        document.getElementById('trWindowOrientation').className = 'hidden';
-    }
-    if (window.screen.orientation && window.screen.orientation.type){
+
+    if (support.windowScreenOrientation){
         document.getElementById('screenOrientationType').textContent = getValue(window.screen.orientation.type);
     }
-    else{
-        document.getElementById('screenOrientationType').textContent = 'not supported';
+    if (support.windowMatchMedia){
+        document.getElementById('windowMatchMediaPortrait').textContent = (window.matchMedia('(orientation: portrait)').matches) ? 'true' : 'false';
+        document.getElementById('windowMatchMediaLandscape').textContent = (window.matchMedia('(orientation: landscape)').matches) ? 'true' : 'false';
     }
-    document.getElementById('windowMatchMediaPortrait').textContent = (window.matchMedia('(orientation: portrait)').matches) ? 'true' : 'false';
-    document.getElementById('windowMatchMediaLandscape').textContent = (window.matchMedia('(orientation: landscape)').matches) ? 'true' : 'false';
     document.getElementById('screenWidth').textContent = window.screen.width;
     document.getElementById('screenHeight').textContent = window.screen.height;
     document.getElementById('screenAvailWidth').textContent = window.screen.availWidth;
